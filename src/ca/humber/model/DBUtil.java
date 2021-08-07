f5fb4352f370c0541f02def7aed399352de41eb8
Abhi Patel <abhipatel181998@gmail.com>
2021-08-07 03:15:00 -0400

    DBUtil and database connection done
    Database connection done. Just getting Internal error while connect using oci driver.

--- src/ca/humber/model/DBUtil.java
+++ src/ca/humber/model/DBUtil.java
@@ -1,5 +1,61 @@
 package ca.humber.model;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class DBUtil {
 
+	private static Connection connection = null;
+	private static Properties properties = new Properties();
+
+	public Connection getConnectionThinDriver() {
+		try {
+			properties.load(new FileInputStream("src/database.properties"));
+
+			Class.forName(properties.getProperty("driver.class.name"));
+
+			connection = DriverManager.getConnection(properties.getProperty("db.url.thin"),
+					properties.getProperty("db.user"), properties.getProperty("db.password"));
+
+		} catch (IOException iEx) {
+			System.out.println("Error in properties file: " + iEx.getMessage());
+		} catch (ClassNotFoundException cEx) {
+			System.out.println("Error in registering databse class: " + cEx.getMessage());
+		} catch (SQLException sEx) {
+			System.out.println("SQL Error: " + sEx.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+
+		return connection;
+	}
+	
+	
+	
+	public Connection getConnectionOciDriver() {
+		try {
+			properties.load(new FileInputStream("src/database.properties"));
+
+			Class.forName(properties.getProperty("driver.class.name"));
+
+			connection = DriverManager.getConnection(properties.getProperty("db.url.oci"),
+					properties.getProperty("db.user"), properties.getProperty("db.password"));
+
+		} catch (IOException iEx) {
+			System.out.println("Error in properties file: " + iEx.getMessage());
+		} catch (ClassNotFoundException cEx) {
+			System.out.println("Error in registering databse class: " + cEx.getMessage());
+		} catch (SQLException sEx) {
+			sEx.printStackTrace();
+			System.out.println("SQL Error: " + sEx.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getSuppressed());
+		}
+
+		return connection;
+	}
 }
--- src/ca/humber/test/HRManagement.java
+++ src/ca/humber/test/HRManagement.java
@@ -1,9 +1,16 @@
 package ca.humber.test;
 
+import ca.humber.model.DBUtil;
+
 public class HRManagement {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		DBUtil dbUtil = new DBUtil();
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 
 	}
 
